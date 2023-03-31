8d109daad25aeb06b5d5561166e063ac626608ef
Thacio Helmer <87128290+thaciohelmer@users.noreply.github.com>
2023-03-31 17:24:28 -0300

    Start Challenges Microservices

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [MongooseModule.forRootAsync(
+    {
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const MG_USER = configService.get<string>('MG_USER')
+        const MG_PASSWORD = configService.get<string>('MG_PASSWORD')
+        const MG_URL = configService.get<string>('MG_URL')
+        const MG_DB_NAME = configService.get<string>('MG_DB_NAME')
+        const uri = `mongodb://${MG_USER}:${MG_PASSWORD}@${MG_URL}`
+        return {
+          uri,
+          dbName: MG_DB_NAME
+        }
+      }
+    }
+  ),
+  ConfigModule.forRoot({ isGlobal: true })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
